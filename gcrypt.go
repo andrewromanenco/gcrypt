4a26ad0887e5fdafe090896c48eac15612f89be0
Andrew Romanenco <andrew.romanenco@shopify.com>
2016-08-16 16:13:06 -0400

    Refactor key generation

--- gcrypt.go
+++ gcrypt.go
@@ -12,15 +12,11 @@ const defaultKeyLen = 32
 // DerivateKey256 creates 256 bit key based on a password. Random salt is
 // returned with the key.
 func DerivateKey256(password string) ([]byte, []byte, error) {
-	if password == "" {
-		return nil, nil, errors.New("Empty pass")
-	}
 	salt, err := generateSalt(defaultKeyLen)
 	if err != nil {
 		return nil, nil, err
 	}
-	// params as recommended
-	key, err := scrypt.Key([]byte(password), salt, 16384, 8, 1, defaultKeyLen)
+	key, err := DerivateKey256WithSalt(password, salt)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -35,6 +31,7 @@ func DerivateKey256WithSalt(password string, salt []byte) ([]byte, error) {
 	if salt == nil || len(salt) != 32 {
 		return nil, errors.New("Salt is not 256 bit")
 	}
+	// Recommended settings for scrypt
 	key, err := scrypt.Key([]byte(password), salt, 16384, 8, 1, defaultKeyLen)
 	if err != nil {
 		return nil, err
